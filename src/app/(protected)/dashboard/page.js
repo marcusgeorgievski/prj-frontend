df8b74c3d81efbd0610d1fa012084859960c491f
Liam Toye <lctoye@myseneca.ca>
2024-05-22 21:48:00 -0400

    dashboard functionality

--- src/app/(protected)/dashboard/page.js
+++ src/app/(protected)/dashboard/page.js
@@ -1,72 +1,144 @@
 // User Dashboard
 
-// UI components
 import PageTitle from "@/components/page-title";
 import Heading from "@/components/heading";
 import { Button } from "@/components/ui/button";
-import { Card, CardHeader } from "@/components/ui/card";
-import { cn } from "@/lib/utils";
 import Link from "next/link";
-import { PiChalkboardTeacherLight } from "react-icons/pi";
-import { PiHouseLineLight } from "react-icons/pi";
-import { PiNotePencilLight } from "react-icons/pi";
-import { PiListChecksLight, PiMagnifyingGlassLight } from "react-icons/pi";
+import {
+  PiChalkboardTeacherLight,
+  PiHouseLineLight,
+  PiNotePencilLight,
+  PiListChecksLight,
+} from "react-icons/pi";
 
-// Components from other pages (Note and Assessment cards not implemented yet)
-import { ClassCard } from "../classes/page";
+import { recentItems } from "@/lib/utils";
+
+import { getClasses, getAssessments, getNotes } from "@/actions/classes";
+import ClassCard from "@/components/classes/class-card";
+// import NoteCard from "@/components/notes/note-card";
+// import AssessmentCard from "@/components/assessments/assessment-card";
 import { NoteCard } from "../notes/page";
 import { AssessmentCard } from "../assessments/page";
+import { currentUser } from "@clerk/nextjs/server";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const { id } = await currentUser();
+
+  // Fetch data
+  const classes = await getClasses(id);
+  // const assessments = await getAssessments(id);
+  // const notes = await getNotes(id);
+  const assessments = [];
+  const notes = [];
+
+
+  // Process recent items
+  const recentClasses = recentItems(classes, 4);
+  const recentAssessments = recentItems(assessments, 4);
+  const recentNotes = recentItems(notes, 4);
+  // Keep track of remaining items
+  const extraClasses = classes.length - recentClasses.length;
+  const extraAssessments = assessments.length - recentAssessments.length;
+  const extraNotes = notes.length - recentNotes.length;
+
   return (
-    <div>
+    <div className="w-full">
       <PageTitle icon={PiHouseLineLight}>Dashboard</PageTitle>
 
       {/* Recent Classes */}
-
-      <div className="w-full mb-8">
+      <div className="w-full mt-8">
         <Heading icon={PiChalkboardTeacherLight}>Recent Classes</Heading>
-        <div className="grid items-center grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          {/* <ClassCard />
-          <ClassCard />
-          <ClassCard /> */}
-        </div>
-        <div className="flex justify-center mt-6">
-          <Link href="/classes/" className="text-lg">
-            <Button>View all classes</Button>
-          </Link>
-        </div>
+        {classes.length === 0 ? (
+          <div className="text-center">
+            <p className="pt-[2vh]">No classes yet!</p>
+            <Link href="/classes/" className="text-lg">
+              <Button className="mt-4">Create a Class</Button>
+            </Link>
+          </div>
+        ) : (
+          <div>
+            <div className="grid items-center grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+              {recentClasses.map((c) => (
+                <div key={c.id}>
+                  <ClassCard key={c.id} {...c} />
+                </div>
+              ))}
+            </div>
+            <div className="flex justify-center mt-6">
+              <Link href="/classes/" className="text-lg">
+                <Button>
+                  {`View All Classes${
+                    extraClasses > 0 ? ` (${extraClasses} more)` : ""
+                  }`}
+                </Button>
+              </Link>
+            </div>
+          </div>
+        )}
       </div>
 
       {/* Recent Assessments */}
-
       <div className="w-full mb-8">
         <Heading icon={PiListChecksLight}>Recent Assessments</Heading>
-        <div className="grid items-center grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          <AssessmentCard />
-          <AssessmentCard />
-          <AssessmentCard />
-        </div>
-        <div className="flex justify-center mt-6">
-          <Link href="/assessments/" className="text-lg">
-            <Button>View all assessments</Button>
-          </Link>
-        </div>
+        {assessments.length === 0 ? (
+          <div className="text-center">
+            <p className="pt-[2vh]">No assessments yet!</p>
+            <Link href="/assessments" className="text-lg">
+              <Button className="mt-4">Create an Assessment</Button>
+            </Link>
+          </div>
+        ) : (
+          <div>
+            <div className="grid items-center grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+              {recentAssessments.map((a) => (
+                <div key={a.id}>
+                  <AssessmentCard key={a.id} {...a} />
+                </div>
+              ))}
+            </div>
+            <div className="flex justify-center mt-6">
+              <Link href="/assessments/" className="text-lg">
+                <Button>
+                  {`View All Assessments${
+                    extraAssessments > 0 ? ` (${extraAssessments} more)` : ""
+                  }`}
+                </Button>
+              </Link>
+            </div>
+          </div>
+        )}
       </div>
 
       {/* Recent Notes */}
       <div className="w-full mb-8">
         <Heading icon={PiNotePencilLight}>Recent Notes</Heading>
-        <div className="grid items-center grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          <NoteCard />
-          <NoteCard />
-          <NoteCard />
-        </div>
-        <div className="flex justify-center mt-6">
-          <Link href="/notes/" className="text-lg">
-            <Button>View all notes</Button>
-          </Link>
-        </div>
+        {notes.length === 0 ? (
+          <div className="text-center">
+            <p className="pt-[2vh]">No notes yet!</p>
+            <Link href="/notes/" className="text-lg">
+              <Button className="mt-4">Create a Note</Button>
+            </Link>
+          </div>
+        ) : (
+          <div>
+            <div className="grid items-center grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+              {recentNotes.map((n) => (
+                <div key={n.id}>
+                  <NoteCard key={n.id} {...n} />
+                </div>
+              ))}
+            </div>
+            <div className="flex justify-center mt-6">
+              <Link href="/notes/" className="text-lg">
+                <Button>
+                  {`View All Notes${
+                    extraNotes > 0 ? ` (${extraNotes} more)` : ""
+                  }`}
+                </Button>
+              </Link>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs))
 }
+
+// Returns the first n most recently updated items from the given array
+export function recentItems(items, maxItems) {
+  return items
+    .sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at))
+    .slice(0, maxItems);
+}
