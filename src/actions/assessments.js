28ca9fab287565e1882135a7cc3bfdbf96b3a3f2
Dai Dung Lam <lamdaidung10c5@gmail.com>
2024-06-17 18:28:23 -0400

    add action for get assessment by user id, get assessment by class id, delete assessment

--- src/actions/assessments.js
+++ src/actions/assessments.js
@@ -1,18 +1,46 @@
-"use server"
+"use server";
 
-import { axiosTokenInstance } from "@/lib/axios"
+import { axiosTokenInstance } from "@/lib/axios";
 
-export async function getUserAssessments(userId = "user_1") {
-    const axios = await axiosTokenInstance()
-  
-    return await axios
-      .get("/assessments", {
-        data: {
-          user_id: userId,
-        },
-      })
-      .then((res) => res.data)
-      .catch((error) => {
-        console.log("ERROR:", error)
-      })
-  }
+export async function getAssessmentsByUserId(userId = "user_1") {
+  const axios = await axiosTokenInstance();
+
+  return await axios
+    .get("/assessments", {
+      data: {
+        user_id: userId,
+      },
+    })
+    .then((res) => res.data)
+    .catch((error) => {
+      console.log("ERROR:", error);
+    });
+}
+
+export async function getAssessmentsByClassId(classId = "1") {
+  const axios = await axiosTokenInstance();
+  return await axios
+    .get(`/classes/${classId}`, {
+      params: {
+        class_id: classId,
+      },
+    })
+    .then((res) => res.data)
+    .catch((error) => {
+      console.log("ERROR:", error);
+    });
+}
+
+export async function deleteAssessment(assessmentId = "assessment_1") {
+  const axios = await axiosTokenInstance()
+  return await axios
+    .delete(`/assessments/${assessmentId}`, {
+      params: {
+        assessment_id: assessmentId,
+      },
+    })
+    .then((res) => res.data)
+    .catch((error) => {
+      console.log("ERROR:", error.response.data)
+    })
+}
