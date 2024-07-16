1840400905796cc917aa644585e7bd19e3b6639c
Marcus Georgievski <marcusgeorgievski@outlook.com>
2024-07-16 14:56:50 -0400

    notes page skeleton and mounting for clerk problems

--- src/app/(protected)/notes/page.js
+++ src/app/(protected)/notes/page.js
@@ -1,43 +1,41 @@
-import PageTitle from "@/components/page-title";
-import { Card, CardHeader } from "@/components/ui/card";
-import { cn } from "@/lib/utils";
-import Link from "next/link";
-import { PiNotePencilLight } from "react-icons/pi";
+import PageTitle from '@/components/page-title'
+import { currentUser } from '@clerk/nextjs/server'
+import { PiNotePencilLight } from 'react-icons/pi'
 
-export default function NotesPage() {
+export default async function NotesPage() {
+  const user = await currentUser()
+
+  if (!user) {
+    return null
+  }
+
+  // simulate fetch with timeout promise
+  const getNotes = async () => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve([
+          { id: 1, title: 'Note 1', content: 'Content 1' },
+          { id: 2, title: 'Note 2', content: 'Content 2' },
+          { id: 3, title: 'Note 3', content: 'Content 3' },
+        ])
+      }, 1000)
+    })
+  }
+
+  const notes = await getNotes(user)
+
   return (
     <div className="w-full ">
       <PageTitle icon={PiNotePencilLight}>Notes</PageTitle>
 
       <div className="grid items-center grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+        <div>a</div>
+        <div>a</div>
+        <div>a</div>
+        <div>a</div>
+        <div>a</div>
+        <div>a</div>
       </div>
     </div>
-  );
-}
-
-export function NoteCard({ noteData }) {
-  return (
-    <Link href={`/notes/#`}>
-      <Card
-        className={cn(
-          "flex flex-col p-3 transition-all hover:bg-accent/50 mx-auto max-w-[500px]"
-        )}
-      >
-        <h3 className="text-lg font-bold">Pointers</h3>
-        <p className="mb-2 text-sm font-light text-muted-foreground">
-          OOP345
-        </p>
-
-        <div className="text-sm font-light">
-          <p>Note Detail</p>
-          <p>Note Content</p>
-        </div>
-      </Card>
-    </Link>
-  );
+  )
 }
